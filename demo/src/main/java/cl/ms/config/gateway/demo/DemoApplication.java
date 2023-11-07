715c4485eb1b5ff33c37eef711317f2b4ccd18bc
luis <luxo.lyr@gmail.com>
2023-11-07 19:28:26 -0300

    fix

--- demo/src/main/java/cl/ms/config/gateway/demo/DemoApplication.java
+++ demo/src/main/java/cl/ms/config/gateway/demo/DemoApplication.java
@@ -2,7 +2,9 @@ package cl.ms.config.gateway.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class DemoApplication {
 
