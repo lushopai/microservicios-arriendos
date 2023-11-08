7172f254725d69472665c31afa3fc0b7ade802ec
luis <luxo.lyr@gmail.com>
2023-11-08 10:53:26 -0300

    fix

---
+++ ms-arriendos/src/main/java/cl/ms/arriendos/MsArriendosApplication.java
@@ -0,0 +1,15 @@
+package cl.ms.arriendos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class MsArriendosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsArriendosApplication.class, args);
+	}
+
+}
---
+++ ms-arriendos/src/test/java/cl/ms/arriendos/MsArriendosApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.ms.arriendos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsArriendosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
