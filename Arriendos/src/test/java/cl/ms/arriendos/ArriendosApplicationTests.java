ebc50561f96b27a3aac2cb7c678e108b4773b4d0
luis <luxo.lyr@gmail.com>
2023-11-07 23:16:30 -0300

    up arriendos

---
+++ Arriendos/src/test/java/cl/ms/arriendos/ArriendosApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.ms.arriendos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArriendosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
